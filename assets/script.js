34f57dd45fb02c3ce1fb4883ea02cfa290d1a3be
Onanike Obinna <onanikefrancis@gmail.com>
2022-11-06 18:20:38 +0000

    js adjustment

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-var topics= ["HTML", "CSS", "Git", "JavaScript"];
-for (var x=0; x < topics.lenght; x++]{
-    console.log(topics[x])
+ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
 }
 /*
 if (topic === 'HTML') {
